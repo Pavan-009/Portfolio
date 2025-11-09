604fb85090a1ac7feb2a567dc5a9b5724a06482e
Pavan_Kumar <pavankumar.99gpk@gmail.com>
2025-11-09 17:25:43 +0530

    encrypted server Link!!!

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import dotenv from 'dotenv';
+dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
       },
     },
   },
   define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://localhost:5000'),
+    'process': JSON.stringify(process.env.VITE_API_URL),
   },
 });
